df02ce96b090595816ed90d7c9d951d1c12fc630
nikolai-y <nikolai.yasiukevich@gmail.com>
2018-03-27 19:16:27 +0300

    get project list with soap

---
+++ test/test_add_project_check_with_soap.py
@@ -0,0 +1,13 @@
+
+def test_add_project(app, data_project):
+    project = data_project
+    old_projects = app.soap.get_project_list(app.config["webadmin"]["username"], app.config["webadmin"]["password"])
+    print(old_projects)
+    assert False
+    # app.session.ensure_login("webadmin")
+    # app.project.add(project)
+    # new_projects = app.soap.get_project_list()
+    # assert len(old_projects) + 1 == len(new_projects)
+    # old_projects.append(project)
+    # assert sorted(old_projects, key=lambda proj: proj.name.lower().replace(' ', '')) == new_projects
+
--- test/test_signup_new_account.py
+++ test/test_signup_new_account.py
@@ -13,6 +13,4 @@ def test_signup_new_account(app):
     email = username + "@localhost"
     app.james.ensure_user_exists(username, password)
     app.signup.new_user(username, email, password)
-    app.session.do_login(username, password)
-    assert app.session.is_logged_in_as(username)
-    app.session.do_logout()
+    assert app.soap.can_login(username, password)
